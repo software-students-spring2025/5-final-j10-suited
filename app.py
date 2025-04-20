086ee79bae9748ffd0595b35bc94715574e486ae
jackinthebox314 <jwangjy315@gmail.com>
2025-04-20 19:21:25 -0400

    added profile page

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 app.secret_key = 'super_secret'
 
 #need to fill in for how to connect to NYU SSO
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
 
 #select groups html page
 @app.route("/select_groups", methods=["GET"])
@@ -29,4 +32,28 @@ def add_group():
 def save_groups():
     selected_groups = request.form.getlist("groups")
     print("User selected:", selected_groups)
-    return "Groups saved!"
+    session['selected_groups'] = selected_groups    # testing profile, should save to database instead
+    flash("Groups saved!", "success")
+    return redirect(url_for("select_groups"))
+
+@app.route("/profile")
+def profile():
+    # get mongodb user from user_id
+    # user = users_collection.find_one({"_id": session["user_id"]})
+    # profile = {
+    #     'name': user['name'],
+    #     'age': user['age'],
+    #     'grade': user['grade'],
+    #     'groups': user['groups']
+    # }
+    profile = {
+        'picture': 'example_image',
+        'name': "Jack",
+        'age': 21,
+        'grade': "Junior",
+        'groups': session.get('selected_groups', 'None joined yet.')
+    }
+    return render_template("profile.html", profile=profile)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
